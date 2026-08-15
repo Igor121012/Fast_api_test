7b04129426c3434509951aa6ac1cc2b64973be50
Igor <kalahyan.2002@bk.ru>
2026-08-15 16:59:42 +0300

    изменил файл database, вместо MySQL используется SQLite

--- database.py
+++ database.py
@@ -1,25 +1,16 @@
-import os
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Загрузка переменных из .env
-load_dotenv()
+# Для SQLite используем локальный файл
+# Можно также использовать :memory: для временной БД в памяти
+SQL_DB_URL = "sqlite:///./app.db"  # или "sqlite:///:memory:" для временной
 
-# Получение данных из окружения
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_NAME = os.getenv('DB_NAME')
+# Для SQLite нужно добавить параметр для поддержки внешних ключей
+engine = create_engine(
+    SQL_DB_URL, 
+    connect_args={"check_same_thread": False}  # нужно для многопоточности
+)
 
-# Формирование URL
-SQL_DB_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-
-# Или более безопасный вариант:
-# SQL_DB_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4'
-
-engine = create_engine(SQL_DB_URL)
 session_local = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 Base = declarative_base()
